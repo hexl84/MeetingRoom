5ebc243e1e1d7be748d5d74d0c79ccd493ab32ac
Henry <157746783@qq.com>
2025-01-26 23:55:43 +0800

    add route and components

---
+++ meeting-room/src/RootRouter.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root, { rootLoader } from "./routes/root";
+import Team, { teamLoader } from "./routes/team";
+import UsersTable from "./Users";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "users",
+        element: <UsersTable />,
+        loader: teamLoader,
+      },
+      {
+        path: "rooms",
+        element: <Team />,
+        loader: teamLoader,
+      },
+      {
+        path: "booking",
+        element: <Team />,
+        loader: teamLoader,
+      },
+      {
+        path: "book-history",
+        element: <Team />,
+        loader: teamLoader,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
