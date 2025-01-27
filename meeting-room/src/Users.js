ef8aa7e3939eb77edc9f62658025717970c93d61
HenryHe <henry@proketportalen.se>
2025-01-27 13:06:06 +0800

    Update user table

--- meeting-room/src/Users.js
+++ meeting-room/src/Users.js
@@ -25,9 +25,9 @@ export default function UsersTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Role</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
             <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
