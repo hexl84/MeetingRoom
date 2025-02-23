339908d838021907635015f180f4a79415c85a00
Henry <157746783@qq.com>
2025-02-23 21:59:58 +0800

    add double click event in the calendar

--- meeting-room/src/components/room/Booking.js
+++ meeting-room/src/components/room/Booking.js
@@ -1,28 +1,48 @@
 import * as React from "react";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import Modal from "@mui/material/Modal";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import periodJson from "../../data/PeriodData.json";
 import roomsJson from "../../data/RoomsData.json";
-import Tooltip from "@mui/material/Tooltip";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
 import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/react'
+import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
 import {
+  viewWeek,
+  viewDay,
+  viewMonthGrid,
+  viewMonthAgenda,
   createViewDay,
   createViewMonthAgenda,
   createViewMonthGrid,
   createViewWeek,
-} from '@schedule-x/calendar'
-import { createEventsServicePlugin } from '@schedule-x/events-service'
- 
-import '@schedule-x/theme-default/dist/index.css'
+} from "@schedule-x/calendar";
+import { createEventsServicePlugin } from "@schedule-x/events-service";
+import { createEventModalPlugin } from "@schedule-x/event-modal";
+import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
+
+import "@schedule-x/theme-default/dist/index.css";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function BookingForm() {
   const navigate = useNavigate();
@@ -35,24 +55,46 @@ function BookingForm() {
   const [end, setEnd] = useState(0);
   const [bookingDate, setBookingDate] = useState(dayjs());
 
-  const eventsService = useState(() => createEventsServicePlugin())[0]
- 
+  const [open, setOpen] = React.useState(false);
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
+
+  const eventsService = useState(() => createEventsServicePlugin())[0];
+
   const calendar = useCalendarApp({
-    views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
+    locale: "en-US",
+    selectedDate: "2025-02-23",
+    defaultView: viewWeek.name,
+    views: [viewDay, viewWeek],
+    plugins: [createEventModalPlugin(), createDragAndDropPlugin()],
     events: [
       {
-        id: '1',
-        title: 'Event 1',
-        start: '2023-12-16',
-        end: '2023-12-16',
+        id: "1",
+        title: "Event 1",
+        start: "2025-02-23 10:00",
+        end: "2025-02-23 11:00",
+      },
+      {
+        id: "2",
+        title: "Event 2",
+        start: "2025-02-23 03:00",
+        end: "2025-02-23 05:00",
       },
     ],
-    plugins: [eventsService]
-  })
+    callbacks: {
+      /**
+       * Is called when clicking somewhere in the time grid of a week or day view
+       * */
+      onDoubleClickDateTime(dateTime) {
+        console.log("onDoubleClickDateTime", dateTime); // e.g. 2024-01-01 12:37
+        handleOpenModal(dateTime);
+      },
+    },
+  });
 
   useEffect(() => {
-    eventsService.getAll();
-    
+    // eventsService.getAll();
+
     const getPeriod = async () => {
       try {
         setCurrentRoom(roomsJson.find((room) => room.id == id));
@@ -78,11 +120,15 @@ function BookingForm() {
   };
   const handleCancelClick = (e) => {
     e.preventDefault();
-    navigate("/rooms");
+    setOpen(false);
+  };
+  const handleDeleteClick = (e) => {
+    e.preventDefault();
+    setOpen(false);
   };
   const handleSaveClick = (e) => {
     e.preventDefault();
-    navigate("/rooms");
+    setOpen(false);
   };
 
   if (loading) return <div>Loading...</div>;
@@ -91,21 +137,44 @@ function BookingForm() {
     <div>
       <h1>Booking {currentRoom.name}</h1>
       <div>
-        <FormControl fullWidth>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label="Booking Date"
-              value={bookingDate}
-              onChange={(newDate) => setBookingDate(newDate)}
+        <ScheduleXCalendar calendarApp={calendar} />
+      </div>
+      <Modal
+        open={open}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div>
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Title"
+              variant="standard"
+              name="title"
+              sx={{ mb: 2 }}
+              required
             />
-          </LocalizationProvider>
+          </div>
+          <div>
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Participants"
+              variant="standard"
+              name="participants"
+              sx={{ mb: 2 }}
+            />
+          </div>
           <InputLabel id="select-start-label">Start</InputLabel>
           <Select
+            fullWidth
             labelId="select-start-label"
             id="start-select"
             value={start}
-            label="Role"
-            name="role"
+            label="Start"
+            name="start"
             onChange={handleStartChange}
           >
             {startPeriods.map((period) => (
@@ -114,17 +183,14 @@ function BookingForm() {
               </MenuItem>
             ))}
           </Select>
-        </FormControl>
-      </div>
-      <div>
-        <FormControl fullWidth>
           <InputLabel id="select-end-label">End</InputLabel>
           <Select
+            fullWidth
             labelId="select-end-label"
             id="end-select"
             value={end}
-            label="Role"
-            name="role"
+            label="End"
+            name="end"
             onChange={handleEndChange}
           >
             {endPeriods.map((period) => (
@@ -133,19 +199,31 @@ function BookingForm() {
               </MenuItem>
             ))}
           </Select>
-        </FormControl>
-      </div>
-      <div>
-        <Button variant="contained" onClick={handleSaveClick}>
-          Book
-        </Button>
-        <Button variant="contained" onClick={handleCancelClick}>
-          Cancel
-        </Button>
-      </div>
-      <div>
-      <ScheduleXCalendar calendarApp={calendar} />
-    </div>
+          <div>
+            <Button
+              variant="contained"
+              onClick={handleDeleteClick}
+              sx={{ mt: 4 }}
+            >
+              Delete
+            </Button>
+            <Button
+              variant="contained"
+              onClick={handleSaveClick}
+              sx={{ mt: 4, marginLeft: 2, float: "right" }}
+            >
+              Book
+            </Button>
+            <Button
+              variant="contained"
+              onClick={handleCancelClick}
+              sx={{ mt: 4, marginLeft: 2, float: "right" }}
+            >
+              Cancel
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }
