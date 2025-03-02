c243a982452241a3f3bd1c82930593e601e78fe6
Henry <157746783@qq.com>
2025-03-02 22:12:08 +0800

    Update booking modal, split it to a new file

---
+++ meeting-room/src/components/BookingModal.js
@@ -0,0 +1,228 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import Modal from "@mui/material/Modal";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import periodJson from "../data/PeriodData.json";
+import dayjs from "dayjs";
+import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
+import { viewWeek, viewDay } from "@schedule-x/calendar";
+import { createEventsServicePlugin } from "@schedule-x/events-service";
+import { createEventModalPlugin } from "@schedule-x/event-modal";
+import { v4 as uuidv4 } from "uuid";
+
+import "@schedule-x/theme-default/dist/index.css";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function BookingModal(props) {
+  const { open, bookingDate, event, setEvent, onCancel, onSave, onDelete } =
+    props;
+
+  const [startPeriods, setStartPeriods] = useState(
+    periodJson.filter((period) => period.id < 19)
+  );
+  const [endPeriods, setEndPeriods] = useState(
+    periodJson.filter((period) => period.id > 1)
+  );
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(0);
+
+  const [title, setTitle] = useState("");
+  const [participants, setParticipants] = useState("");
+
+  useEffect(() => {
+    // const initEvent = async () => {
+    //   try {
+    //     if (event) {
+    //       setTitle(event.title);
+    //       setParticipants(event.participants);
+    //       const startHour = dayjs(event.start).format("HH:mm");
+    //       const endHour = dayjs(event.end).format("HH:mm");
+    //       const startIndex = periodJson.find(
+    //         (period) => period.timestamp === startHour
+    //       ).id;
+    //       const endIndex = periodJson.find(
+    //         (period) => period.timestamp === endHour
+    //       ).id;
+    //       setStart(startIndex);
+    //       setEnd(endIndex);
+    //     } else {
+    //       setEvent({
+    //         title: "",
+    //         participants: "",
+    //         start: dayjs(event.start),
+    //         end: dayjs(event.end),
+    //       });
+    //     }
+    //   } catch (error) {
+    //     console.log("Error:", error);
+    //   }
+    // };
+    // initEvent();
+  }, []);
+
+  const handleStartChange = (e) => {
+    // setStart(e.target.value);
+    setEvent({ ...event, ["start"]: e.target.value });
+    setEndPeriods(periodJson.filter((period) => period.id > e.target.value));
+  };
+  const handleEndChange = (e) => {
+    setEvent({ ...event, ["end"]: e.target.value });
+  };
+
+  const handleTitleChange = (e) => {
+    // setTitle(e.target.value);
+    const { name, value } = e.target;
+    setEvent({ ...event, [name]: value });
+  };
+
+  const handleParticipantsChange = (e) => {
+    // setParticipants(e.target.value);
+    setEvent({ ...event, ["participants"]: e.target.value });
+  };
+
+  const handleCancelClick = (e) => {
+    e.preventDefault();
+    onCancel();
+  };
+
+  const handleDeleteClick = (e) => {
+    e.preventDefault();
+    onDelete();
+  };
+
+  const handleSaveClick = (e) => {
+    e.preventDefault();
+
+    const startHour = startPeriods.find(
+      (period) => period.id == event.start
+    ).timestamp;
+    const endHour = endPeriods.find(
+      (period) => period.id == event.end
+    ).timestamp;
+
+    const newEvent = {
+      id: uuidv4(),
+      title: event.title,
+      participants: event.participants,
+      start: bookingDate.format("YYYY-MM-DD") + " " + startHour,
+      end: bookingDate.format("YYYY-MM-DD") + " " + endHour,
+    };
+    setEvent(newEvent);
+    onSave(newEvent);
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleCancelClick}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <div>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Title"
+            variant="standard"
+            name="title"
+            value={event?.title}
+            sx={{ mb: 2 }}
+            required
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Participants"
+            variant="standard"
+            name="participants"
+            value={event?.participants}
+            sx={{ mb: 2 }}
+            required
+            onChange={handleParticipantsChange}
+          />
+        </div>
+        <InputLabel id="select-start-label">Start</InputLabel>
+        <Select
+          fullWidth
+          labelId="select-start-label"
+          id="start-select"
+          value={event?.start}
+          label="Start"
+          name="event.start"
+          required
+          onChange={handleStartChange}
+        >
+          {startPeriods.map((period) => (
+            <MenuItem key={period.id} value={period.id}>
+              {period.timestamp}
+            </MenuItem>
+          ))}
+        </Select>
+        <InputLabel id="select-end-label">End</InputLabel>
+        <Select
+          fullWidth
+          labelId="select-end-label"
+          id="end-select"
+          value={event?.end}
+          label="End"
+          name="event.end"
+          required
+          onChange={handleEndChange}
+        >
+          {endPeriods.map((period) => (
+            <MenuItem key={period.id} value={period.id}>
+              {period.timestamp}
+            </MenuItem>
+          ))}
+        </Select>
+        <div>
+          <Button
+            variant="contained"
+            onClick={handleDeleteClick}
+            sx={{ mt: 4 }}
+          >
+            Delete
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleSaveClick}
+            sx={{ mt: 4, marginLeft: 2, float: "right" }}
+          >
+            Book
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleCancelClick}
+            sx={{ mt: 4, marginLeft: 2, float: "right" }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </Box>
+    </Modal>
+  );
+}
+
+export default BookingModal;
--- meeting-room/src/pages/room/Booking.js
+++ meeting-room/src/pages/room/Booking.js
@@ -1,11 +1,4 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Select from "@mui/material/Select";
-import Typography from "@mui/material/Typography";
-import MenuItem from "@mui/material/MenuItem";
-import InputLabel from "@mui/material/InputLabel";
 import Modal from "@mui/material/Modal";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -19,31 +12,15 @@ import { createEventModalPlugin } from "@schedule-x/event-modal";
 import { v4 as uuidv4 } from "uuid";
 
 import "@schedule-x/theme-default/dist/index.css";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import BookingModal from "../../components/BookingModal";
 
 function BookingForm() {
   const navigate = useNavigate();
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
-  const [startPeriods, setStartPeriods] = useState([]);
-  const [endPeriods, setEndPeriods] = useState([]);
   const [currentRoom, setCurrentRoom] = useState();
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(0);
+
   const [bookingDate, setBookingDate] = useState(dayjs());
-  const [title, setTitle] = useState("");
-  const [participants, setParticipants] = useState("");
   const defaulEvents = [
     {
       id: "1",
@@ -60,10 +37,10 @@ function BookingForm() {
   ];
 
   const [events, setEvents] = useState(defaulEvents);
+  const [currentEvent, setCurrentEvent] = useState(null);
 
   const [open, setOpen] = React.useState(false);
-  const handleOpenModal = () => setOpen(true);
-  const handleCloseModal = () => setOpen(false);
+
   // const handleOpenEditModal = (event) => {
   //   setOpen(true);
   //   setTitle(event.title);
@@ -99,9 +76,16 @@ function BookingForm() {
        * Is called when clicking somewhere in the time grid of a week or day view
        * */
       onDoubleClickDateTime(dateTime) {
-        console.log("onDoubleClickDateTime", dateTime); // e.g. 2024-01-01 12:37
-        handleOpenModal();
+        // console.log("onDoubleClickDateTime", dateTime); // e.g. 2024-01-01 12:37
         setBookingDate(dayjs(dateTime));
+        setCurrentEvent({
+          title: "",
+          participants: "",
+          start: dateTime,
+          end: dateTime,
+        });
+        console.log(currentEvent);
+        setOpen(true);
       },
       // onDoubleClickEvent(event) {
       //   console.log("onDoubleClickEvent", event);
@@ -110,14 +94,30 @@ function BookingForm() {
     },
   });
 
+  const handleBookingModalCancel = (e) => {
+    setCurrentEvent(null);
+    setOpen(false);
+  };
+
+  const handleBookingModalDelete = (event) => {
+    calendar.events.remove(event.Id);
+    setCurrentEvent(null);
+    setOpen(false);
+  };
+
+  const handleBookingModalSave = (event) => {
+    calendar.events.add(event);
+    setCurrentEvent(null);
+    setOpen(false);
+    // event = {};
+  };
+
   useEffect(() => {
     // eventsService.getAll();
 
     const getPeriod = async () => {
       try {
         setCurrentRoom(roomsJson.find((room) => room.id == id));
-        setStartPeriods(periodJson.filter((period) => period.id < 19));
-        setEndPeriods(periodJson.filter((period) => period.id > 1));
         setLoading(false);
       } catch (error) {
         console.log("Error:", error);
@@ -127,50 +127,6 @@ function BookingForm() {
     getPeriod();
   }, [id]);
 
-  const handleStartChange = (e) => {
-    setStart(e.target.value);
-    setEndPeriods(periodJson.filter((period) => period.id > e.target.value));
-  };
-  const handleEndChange = (e) => {
-    setEnd(e.target.value);
-  };
-
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleParticipantsChange = (e) => {
-    setParticipants(e.target.value);
-  };
-
-  const handleCancelClick = (e) => {
-    e.preventDefault();
-    setOpen(false);
-  };
-  const handleDeleteClick = (e) => {
-    e.preventDefault();
-    setOpen(false);
-  };
-  const handleSaveClick = (e) => {
-    e.preventDefault();
-
-    const startHour = startPeriods.find(
-      (period) => period.id == start
-    ).timestamp;
-    const endHour = endPeriods.find((period) => period.id == end).timestamp;
-
-    const newEvent = {
-      id: uuidv4(),
-      title: title,
-      participants: participants,
-      start: bookingDate.format("YYYY-MM-DD") + " " + startHour,
-      end: bookingDate.format("YYYY-MM-DD") + " " + endHour,
-    };
-
-    calendar.events.add(newEvent);
-    setOpen(false);
-  };
-
   if (loading) return <div>Loading...</div>;
 
   return (
@@ -179,95 +135,15 @@ function BookingForm() {
       <div>
         <ScheduleXCalendar calendarApp={calendar} />
       </div>
-      <Modal
+      <BookingModal
         open={open}
-        onClose={handleCloseModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <div>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="Title"
-              variant="standard"
-              name="title"
-              value={title}
-              sx={{ mb: 2 }}
-              required
-              onChange={handleTitleChange}
-            />
-          </div>
-          <div>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="Participants"
-              variant="standard"
-              name="participants"
-              value={participants}
-              sx={{ mb: 2 }}
-              onChange={handleParticipantsChange}
-            />
-          </div>
-          <InputLabel id="select-start-label">Start</InputLabel>
-          <Select
-            fullWidth
-            labelId="select-start-label"
-            id="start-select"
-            value={start}
-            label="Start"
-            name="start"
-            onChange={handleStartChange}
-          >
-            {startPeriods.map((period) => (
-              <MenuItem key={period.id} value={period.id}>
-                {period.timestamp}
-              </MenuItem>
-            ))}
-          </Select>
-          <InputLabel id="select-end-label">End</InputLabel>
-          <Select
-            fullWidth
-            labelId="select-end-label"
-            id="end-select"
-            value={end}
-            label="End"
-            name="end"
-            onChange={handleEndChange}
-          >
-            {endPeriods.map((period) => (
-              <MenuItem key={period.id} value={period.id}>
-                {period.timestamp}
-              </MenuItem>
-            ))}
-          </Select>
-          <div>
-            <Button
-              variant="contained"
-              onClick={handleDeleteClick}
-              sx={{ mt: 4 }}
-            >
-              Delete
-            </Button>
-            <Button
-              variant="contained"
-              onClick={handleSaveClick}
-              sx={{ mt: 4, marginLeft: 2, float: "right" }}
-            >
-              Book
-            </Button>
-            <Button
-              variant="contained"
-              onClick={handleCancelClick}
-              sx={{ mt: 4, marginLeft: 2, float: "right" }}
-            >
-              Cancel
-            </Button>
-          </div>
-        </Box>
-      </Modal>
+        bookingDate={bookingDate}
+        event={currentEvent}
+        setEvent={setCurrentEvent}
+        onCancel={handleBookingModalCancel}
+        onSave={handleBookingModalSave}
+        onDelete={handleBookingModalDelete}
+      ></BookingModal>
     </div>
   );
 }
