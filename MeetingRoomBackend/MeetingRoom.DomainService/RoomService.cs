a1aa031768226951a9fc1c022cf793ddd46be53c
Henry <157746783@qq.com>
2025-04-12 23:05:34 +0800

    add transaction

--- MeetingRoomBackend/MeetingRoom.DomainService/RoomService.cs
+++ MeetingRoomBackend/MeetingRoom.DomainService/RoomService.cs
@@ -1,51 +1,92 @@
 ﻿using MeetingRoom.DTO.Request;
 using MeetingRoom.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeetingRoom.DomainService
 {
     public class RoomService : IRoomService
     {
         private readonly IRoomRepository _roomRepository;
+        private readonly MeetingRoomContext _context;
 
-        public RoomService(IRoomRepository roomRepository)
+        public RoomService(IRoomRepository roomRepository, MeetingRoomContext context)
         {
             _roomRepository = roomRepository;
+            _context = context;
         }
 
         public void AddRoom(RoomRequest request)
         {
-            var existRoom = _roomRepository.GetByName(request.Name);
-            if (existRoom != null)
+            using var transaction = _context.Database.BeginTransaction();
+            try
             {
-                throw new Exception("Room already exists");
+                var existRoom = _roomRepository.GetByName(request.Name);
+                if (existRoom != null)
+                {
+                    throw new Exception("Room already exists");
+                }
+
+                var newRoom = Domain.Room.Create(request.Name, request.Capacity, request.Status, request.Type, request.Comment);
+                _roomRepository.Add(newRoom);
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
             }
 
-            var newRoom = Domain.Room.Create(request.Name, request.Capacity, request.Status, request.Type, request.Comment);
-            _roomRepository.Add(newRoom);
+
         }
 
         public void DeleteRoom(int id)
         {
-            var existRoom = _roomRepository.Get(id);
-            if (existRoom == null)
+            using var transaction = _context.Database.BeginTransaction();
+            try
             {
-                throw new Exception("Room not found");
+                var existRoom = _roomRepository.Get(id);
+                if (existRoom == null)
+                {
+                    throw new Exception("Room not found");
+                }
+
+                _roomRepository.Delete(id);
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
             }
 
-            _roomRepository.Delete(id);
         }
 
         public void EditRoom(RoomRequest request)
         {
-            var existRoom = _roomRepository.Get(request.RoomId);
-            if (existRoom == null)
+            using var transaction = _context.Database.BeginTransaction();
+            try
             {
-                throw new Exception("Room not found");
+                var existRoom = _roomRepository.Get(request.RoomId);
+                if (existRoom == null)
+                {
+                    throw new Exception("Room not found");
+                }
+
+                existRoom.Update(request.Name, request.Capacity, request.Status, request.Type, request.Comment);
+
+                _roomRepository.Update(existRoom);
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
             }
 
-            existRoom.Update(request.Name, request.Capacity, request.Status, request.Type, request.Comment);
 
-            _roomRepository.Update(existRoom);
         }
     }
 }
--- MeetingRoomBackend/MeetingRoom.DomainService/UserService.cs
+++ MeetingRoomBackend/MeetingRoom.DomainService/UserService.cs
@@ -6,45 +6,80 @@ namespace MeetingRoom.DomainService
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
-        public UserService(IUserRepository userRepository)
+        private readonly MeetingRoomContext _context;
+
+        public UserService(IUserRepository userRepository, MeetingRoomContext context)
         {
             _userRepository = userRepository;
+            _context = context;
         }
 
         public void AddUser(UserRequest request)
         {
-            var existUser = _userRepository.GetByName(request.Name);
-            if (existUser != null)
+            using var transaction = _context.Database.BeginTransaction();
+            try
             {
-                throw new Exception("User already exists");
+                var existUser = _userRepository.GetByName(request.Name);
+                if (existUser != null)
+                {
+                    throw new Exception("User already exists");
+                }
+
+                var newUser = Domain.User.Create(request.Name, request.Email, request.Phone, request.RoleId);
+                _userRepository.Add(newUser);
+
+                transaction.Commit();
             }
-
-            var newUser = Domain.User.Create(request.Name, request.Email, request.Phone, request.RoleId);
-            _userRepository.Add(newUser);
-
-        }
-
-        public void DeleteUser(int id)
-        {
-            var existUser = _userRepository.Get(id);
-            if (existUser == null)
+            catch
             {
-                throw new Exception("User not exists");
+                transaction.Rollback();
+                throw;
             }
-
-            _userRepository.Delete(id);
         }
 
         public void EditUser(UserRequest request)
         {
-            var existUser = _userRepository.Get(request.UserId);
-            if (existUser == null)
+            using var transaction = _context.Database.BeginTransaction();
+            try
             {
-                throw new Exception("User not exists");
-            }
+                var existUser = _userRepository.Get(request.UserId);
+                if (existUser == null)
+                {
+                    throw new Exception("User not exists");
+                }
 
-            existUser.Update(request.Name, request.Email, request.Phone, request.RoleId);
-            _userRepository.Update(existUser);  
+                existUser.Update(request.Name, request.Email, request.Phone, request.RoleId);
+                _userRepository.Update(existUser);
+
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+        public void DeleteUser(int id)
+        {
+            using var transaction = _context.Database.BeginTransaction();
+            try
+            {
+                var existUser = _userRepository.Get(id);
+                if (existUser == null)
+                {
+                    throw new Exception("User not exists");
+                }
+
+                _userRepository.Delete(id);
+
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
         }
     }
 }
